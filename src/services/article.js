a52e3e45345139c69b038830df7e8e5f732f8f67
Aman Kumar <129372288+amank42@users.noreply.github.com>
2023-07-14 17:15:52 +0530

    added AI Summarizer

---
+++ src/services/article.js
@@ -0,0 +1,37 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+const rapidApiKeys = import.meta.env.VITE_RAPID_API_ARTICLE_KEY;
+
+export const articleApi = createApi({
+    reducerPath: 'articleApi',
+    baseQuery: fetchBaseQuery({
+        baseUrl: 'https://article-extractor-and-summarizer.p.rapidapi.com/',
+        prepareHeaders: (headers) => {
+            headers.set('X-RapidAPI-Key', rapidApiKeys)
+            headers.set('X-RapidAPI-Host', 'article-extractor-and-summarizer.p.rapidapi.com');
+
+            return headers;
+        }
+    }),
+    endpoints: (builder) => ({
+        getSummary: builder.query({
+            query: (params) => `/summarize?url=${encodeURIComponent(params.articleUrl)}&length=3`
+        })
+    })
+});
+
+export const { useLazyGetSummaryQuery } = articleApi;
+
+
+// const options = {
+//     method: 'GET',
+//     url: 'https://article-extractor-and-summarizer.p.rapidapi.com/summarize',
+//     params: {
+//       url: 'https://time.com/6266679/musk-ai-open-letter/',
+//       length: '3'
+//     },
+//     headers: {
+//       'X-RapidAPI-Key': 'cb14217aebmsh2a0b47b5cc95da1p1c8930jsnace804a107b2',
+//       'X-RapidAPI-Host': 'article-extractor-and-summarizer.p.rapidapi.com'
+//     }
+//   };
